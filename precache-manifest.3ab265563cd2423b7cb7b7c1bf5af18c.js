0e957dda97ec0edc9cd0b32e13dc468ae2ecaa43
Admin <thang123lk@gmail.com>
2020-04-26 21:27:07 +0700

    Updates

---
+++ precache-manifest.3ab265563cd2423b7cb7b7c1bf5af18c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2037ab1865ab8dcd8f49d17e0cdc3c",
+    "url": "/covid-19-statistics/index.html"
+  },
+  {
+    "revision": "41f60697186a5ef4dd77",
+    "url": "/covid-19-statistics/static/css/main.5369f41c.chunk.css"
+  },
+  {
+    "revision": "1dbf8236ce08c2446d37",
+    "url": "/covid-19-statistics/static/js/2.87f93da0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-statistics/static/js/2.87f93da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f60697186a5ef4dd77",
+    "url": "/covid-19-statistics/static/js/main.21d954ec.chunk.js"
+  },
+  {
+    "revision": "8272d8346c0833905e92",
+    "url": "/covid-19-statistics/static/js/runtime-main.f5059b9d.js"
+  },
+  {
+    "revision": "5d6b67a56990e5998be352513ae236b8",
+    "url": "/covid-19-statistics/static/media/breathshortness.5d6b67a5.png"
+  },
+  {
+    "revision": "7ded61d4d553f54b4462db37e1be44f6",
+    "url": "/covid-19-statistics/static/media/fever.7ded61d4.png"
+  },
+  {
+    "revision": "4d9e5edc9e3d9e8928436e2a6c101755",
+    "url": "/covid-19-statistics/static/media/homecare.4d9e5edc.png"
+  },
+  {
+    "revision": "db080224bee459288363a7f77c9d2074",
+    "url": "/covid-19-statistics/static/media/homecare2.db080224.png"
+  },
+  {
+    "revision": "1f41160eeca05a6127c5a8a3b4fadd22",
+    "url": "/covid-19-statistics/static/media/homecare3.1f41160e.png"
+  },
+  {
+    "revision": "362a7d0765478c3293236d03ac1acffe",
+    "url": "/covid-19-statistics/static/media/homecare4.362a7d07.png"
+  },
+  {
+    "revision": "b3848d66c74e5302c4536c05d2120597",
+    "url": "/covid-19-statistics/static/media/homecare5.b3848d66.png"
+  },
+  {
+    "revision": "71fc18b44f3b6e6c8e0a647d9db5387f",
+    "url": "/covid-19-statistics/static/media/homecare6.71fc18b4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-statistics/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8c5f6b7324f0b62e2c5ffc89c661935",
+    "url": "/covid-19-statistics/static/media/protect.f8c5f6b7.png"
+  },
+  {
+    "revision": "f3ba1098527f730da3a0f00397a34ca8",
+    "url": "/covid-19-statistics/static/media/protect2.f3ba1098.png"
+  },
+  {
+    "revision": "2bcf50cb67f875df650b909be99e266c",
+    "url": "/covid-19-statistics/static/media/protect3.2bcf50cb.png"
+  },
+  {
+    "revision": "028875c19a47ee350d0f61ccc8bf6d40",
+    "url": "/covid-19-statistics/static/media/stress.028875c1.jpg"
+  },
+  {
+    "revision": "13dab13e84ea653e06abe345d32665b5",
+    "url": "/covid-19-statistics/static/media/stress2.13dab13e.jpg"
+  },
+  {
+    "revision": "aa5e6c2649f1f4afe6bb12306b3d953d",
+    "url": "/covid-19-statistics/static/media/virus.aa5e6c26.jpg"
+  },
+  {
+    "revision": "71d4ff9b4f39c7e84e25f78ad8b400fb",
+    "url": "/covid-19-statistics/static/media/virus2.71d4ff9b.jpg"
+  }
+]);
