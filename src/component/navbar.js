4d7304f791873e4c6b7938c4168a19afb6e4279b
Admin <thang123lk@gmail.com>
2020-04-26 17:54:40 +0700

    update navbar logo

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return(
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <a class="navbar-brand" href="/">COVID-19</a>
+            <a class="navbar-brand" href="/"><i class="fas fa-lungs-virus"></i> COVID-19</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
